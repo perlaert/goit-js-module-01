685b6bf417e02e8c6b98b2c879a78f901a05b259
Katerina Kucheriavenko <ekucheryavenko0925@gmail.com>
2020-10-13 00:42:18 +0300

    Удаляет условие задачи

--- js/script-4.js
+++ js/script-4.js
@@ -2,8 +2,8 @@ const orderPieces = 4;
 
 const credits = 23580;
 const pricePerDroid = 3000;
-const CANCELED_BY_USER = "Отменено пользователем!";
-const ACCESS_DENIED = "Недостаточно средств на счету!";
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_DENIED = 'Недостаточно средств на счету!';
 
 let totalPrice = pricePerDroid * orderPieces; // Write code on this line
 let balanceCredit = credits - totalPrice; // Write code on this line
@@ -11,23 +11,11 @@ let message;
 
 // Write code under this line
 if (orderPieces === null) {
-  message = CANCELED_BY_USER;
+    message = CANCELED_BY_USER;
 } else if (totalPrice <= credits) {
-  message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
+    message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
 } else if (totalPrice > credits) {
-  message = ACCESS_DENIED;
+    message = ACCESS_DENIED;
 }
 
 console.log(message);
-
-//если orderPieces равно 4
-// то значение message будет равно
-// 'Вы купили 4 дроидов, на счету осталось 11580 кредитов'
-
-//если orderPieces равно null
-// то значение message будет равно
-// 'Отменено пользователем!'
-
-//если orderPieces равно 10
-// то значение message будет равно
-// 'Недостаточно средств на счету!'
--- js/script-5.js
+++ js/script-5.js
@@ -1,55 +1,43 @@
-const countryName = "КитаЙ";
+const countryName = 'КитаЙ';
 
-const CANCELED_BY_USER = "Отменено пользователем!";
-const NO_DELIVERY = "В выбранную страну доставка недоступна.";
-const CHINA = "Китай";
-const AUSTRALIA = "Австралия";
-const INDIA = "Индия";
-const JAMAICA = "Ямайка";
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
+const CHINA = 'Китай';
+const AUSTRALIA = 'Австралия';
+const INDIA = 'Индия';
+const JAMAICA = 'Ямайка';
 let message;
 let price = 0;
 let country;
 if (countryName === null) {
-  message = CANCELED_BY_USER;
+    message = CANCELED_BY_USER;
 } else {
-  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase(); // Write code on this line
-  switch (country) {
-    case "Китай":
-      price = 100;
-      break;
+    country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase(); // Write code on this line
+    switch (country) {
+        case 'Китай':
+            price = 100;
+            break;
 
-    case "Австралия":
-      price = 170;
-      break;
+        case 'Австралия':
+            price = 170;
+            break;
 
-    case "Индия":
-      price = 80;
-      break;
+        case 'Индия':
+            price = 80;
+            break;
 
-    case "Ямайка":
-      price = 120;
-      break;
+        case 'Ямайка':
+            price = 120;
+            break;
 
-    default:
-      message = NO_DELIVERY;
-    // Write code under this line
-  }
+        default:
+            message = NO_DELIVERY;
+        // Write code under this line
+    }
 }
 if (price > 0) {
-  // Write code on this line
-  message = `Доставка в ${country} будет стоить ${price} кредитов`;
+    // Write code on this line
+    message = `Доставка в ${country} будет стоить ${price} кредитов`;
 }
 
 console.log(message);
-
-//если countryName равно "КитаЙ"
-// то значение message будет равно
-// 'Доставка в Китай будет стоить 100 кредитов'
-
-//если countryName равно null
-// то значение message будет равно
-// 'Отменено пользователем!'
-
-//если countryName равно "Чили"
-// то значение message будет равно
-// 'В выбранную страну доставка недоступна.'
